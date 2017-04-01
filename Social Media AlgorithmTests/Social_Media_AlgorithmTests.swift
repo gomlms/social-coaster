11bc0fa66402a4787c6f1b5e146feeb53c897c78
Max Sidebotham <gomlms@wireless-10-104-134-77.umd.edu>
2017-03-31 21:11:20 -0400

    Initial Commit

---
+++ Social Media AlgorithmTests/Social_Media_AlgorithmTests.swift
@@ -0,0 +1,36 @@
+//
+//  Social_Media_AlgorithmTests.swift
+//  Social Media AlgorithmTests
+//
+//  Created by Max Sidebotham on 3/31/17.
+//  Copyright © 2017 Sandbox. All rights reserved.
+//
+
+import XCTest
+@testable import Social_Media_Algorithm
+
+class Social_Media_AlgorithmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
